2e078f124c75578a2b5e11d01f39b2a127985edd
leemaHamid <leemahamid99@gmail.com>
2024-09-04 16:15:21 +0000

    created model

--- iris_dataset.py
+++ iris_dataset.py
@@ -4,8 +4,9 @@ import pandas as pd
 iris = datasets.load_iris()
 iris_df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
 
-# Add the target column to the DataFrame
 iris_df['target'] = iris.target
 
-# Print the first few rows of the DataFrame
+
 print(iris_df.head())
+
+ 
---
+++ logistiRegression.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from  iris_dataset import iris
+
+# Load the Iris dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+# Perform a train-test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Train the Logistic Regression model
+model = LogisticRegression(max_iter=200)
+model.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = model.predict(X_test)
+
+# Evaluate the model's accuracy
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Model Accuracy: {accuracy:.2f}")
